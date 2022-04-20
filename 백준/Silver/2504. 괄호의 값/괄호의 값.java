cfc6bf944820dfd3dffa73f7814267399000ee63
nicebyy <45655434+nicebyy@users.noreply.github.com>
2022-04-21 00:19:12 +0900

    [Silver II] Title: 괄호의 값, Time: 132 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Silver/2504. 괄호의 값/괄호의 값.java
+++ 백준/Silver/2504. 괄호의 값/괄호의 값.java
@@ -22,23 +22,23 @@ public class Main {
         int answer = 0;
         Stack<String> stack = new Stack<>();
         String prev = "";
-        int cur = 1;
+        int cur = 1; // 1에서 부터 시작
 
         for (String word : words) {
-
-            if(word.equals("(")){
+            
+            if(word.equals("(")){  // 열리는 괄호 일때 
                 cur *=2;
                 stack.push(word);
             }else if(word.equals("[")){
                 cur *= 3;
                 stack.push(word);
-            }else if(word.equals(")")){
+            }else if(word.equals(")")){ // 닫히는 괄호 일때
 
-                if(stack.isEmpty() || !stack.peek().equals("(")){
+                if(stack.isEmpty() || !stack.peek().equals("(")){ // 잘못 된 괄호이면 break
                     answer=0;
                     break;
                 }
-                else if(prev.equals("("))
+                else if(prev.equals("(")) // 중간 정산 
                     answer += cur;
 
                 stack.pop();
@@ -55,14 +55,12 @@ public class Main {
                 stack.pop();
                 cur /= 3;
             }
-            prev = word;
+            prev = word; // 중간 정산을 위한 이전 값 기록
         }
 
-        if(!stack.isEmpty())
+        if(!stack.isEmpty()) // 괄호짝이 남는 경우 => 잘못된 괄호
             answer = 0;
         System.out.println(answer);
     }
-
-
 }
 
