236291523260af62de446e8605f4e5f1563cbadc
nicebyy <45655434+nicebyy@users.noreply.github.com>
2022-04-22 01:17:34 +0900

    [level 2] Title: 주식가격, Time: 9.26 ms, Memory: 62.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for(int i=0;i<prices.length;i++){
+
+            int curPrice = prices[i];
+            int time=0;
+
+            for(int j=i+1;j<prices.length;j++){
+                int nextPrice = prices[j];
+                time++;
+                if(curPrice>nextPrice){
+                    break;
+                }
+            }
+
+            answer[i]=time;
+        }
+        return answer;
+    }
+}
