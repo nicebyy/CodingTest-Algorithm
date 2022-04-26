4dc18aa0876e83ac0e760cd98adb13934b7cfa62
nicebyy <45655434+nicebyy@users.noreply.github.com>
2022-04-27 00:53:18 +0900

    [Silver I] Title: 에너지 모으기, Time: 168 ms, Memory: 15928 KB -BaekjoonHub

--- 백준/Silver/16198. 에너지 모으기/에너지 모으기.java
+++ 백준/Silver/16198. 에너지 모으기/에너지 모으기.java
@@ -14,6 +14,7 @@ public class Main {
     static int[] energy;
     static boolean[] visit;
     static int[] next;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -22,47 +23,42 @@ public class Main {
                 .mapToInt(Integer::parseInt).toArray();
         visit = new boolean[n];
         next = new int[n];
-        for(int i=0;i<n;i++)
-            next[i]=i;
+        for (int i = 0; i < n; i++)
+            next[i] = i; // 현재 위치는 자기 자신
 
-        getEnergy(0,0);
+        getEnergy(0, 0);
         System.out.println(maxEnergy);
     }
 
-    public static void getEnergy(int depth,int totalEnergy){
+    public static void getEnergy(int depth, int totalEnergy) {
 
-//        System.out.print("curEnergy = "+totalEnergy);
-//        System.out.println("     depth = "+depth);
-
-        if(depth==n-2){
-            maxEnergy = Math.max(totalEnergy,maxEnergy);
+        if (depth == n - 2) { // 최대값 갱신
+            maxEnergy = Math.max(totalEnergy, maxEnergy);
         }
 
-        for(int i=1;i<n-1;i++){
+        for (int i = 1; i < n - 1; i++) {
 
-            if(!visit[i]){
-                visit[i]=true;
-                getEnergy(depth+1,totalEnergy+(energy[findRight(i)]*energy[findLeft(i)]));
-
-                visit[i]=false;
-                next[i]=i;
+            if (!visit[i]) { // 만약 방문 안한 곳이면
+                visit[i] = true;
+                getEnergy(depth + 1, totalEnergy +
+                        energy[findRight(i)] * energy[findLeft(i)]); // 좌우로 방문된 곳 제외하고 새로 찾은 값
+                visit[i] = false;
+                next[i] = i;
             }
         }
     }
 
-    public static int findRight(int x){
+    public static int findRight(int x) {
 
-        if(!visit[x]){
+        if (!visit[x]) // 현재 위치가 방문안한 곳이면 해당 인덱스 return
             return x;
-        }
-        return next[x] = findRight(x+1);
+        return next[x] = findRight(x + 1); // 다음 위치 return
     }
 
-    public static int findLeft(int x){
+    public static int findLeft(int x) {
 
-        if(!visit[x]){
+        if (!visit[x])
             return x;
-        }
-        return next[x] = findLeft(x-1);
+        return next[x] = findLeft(x - 1);
     }
 }
