8adb8959c70536c79126956af00b1a21060c41b9
nicebyy <45655434+nicebyy@users.noreply.github.com>
2022-05-06 01:21:07 +0900

    [Silver III] Title: 사탕 게임, Time: 260 ms, Memory: 16444 KB -BaekjoonHub

---
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -0,0 +1,96 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int max = 1;
+    static int n;
+    static char[][] map;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        map = new char[n][n];
+
+        for(int i=0;i<n;i++){
+            map[i] = br.readLine().toCharArray();
+        } // end input
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+
+                if(isRange(j+1,i)) {
+                    swap(new Point(j,i),new Point(j+1,i));
+                }
+
+                if(isRange(j,i+1)) {
+                    swap(new Point(j,i),new Point(j,i+1));
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+
+    private static void swap(Point p1,Point p2) {  // 0:x 1:y
+        char cur = map[p1.y][p1.x];
+        char next = map[p2.y][p2.x];
+//        System.out.println("===================before==============");
+//        System.out.print("map["+p1.y+"]["+p1.x+"] = " + map[p1.y][p1.x]);
+//        System.out.println("     map["+p2.y+"]["+p2.x+"] = " + map[p2.y][p2.x]);
+
+        map[p1.y][p1.x] = next;
+        map[p2.y][p2.x] = cur;
+//        System.out.println("===================after==============");
+//        System.out.print("map["+p1.y+"]["+p1.x+"] = " + map[p1.y][p1.x]);
+//        System.out.println("     map["+p2.y+"]["+p2.x+"] = " + map[p2.y][p2.x]);
+
+        search();
+        map[p1.y][p1.x] = cur;
+        map[p2.y][p2.x] = next;
+//        System.out.println("===================restore==============");
+//        System.out.print("map["+p1.y+"]["+p1.x+"] = " + map[p1.y][p1.x]);
+//        System.out.println("     map["+p2.y+"]["+p2.x+"] = " + map[p2.y][p2.x]);
+    }
+
+    private static void search() {
+
+        int cnt = 1;
+
+        for(int i=0;i<n;i++){ // x 방향 진행
+            for(int j=0;j<n;j++){
+
+                if(isRange(j+1,i) && map[i][j]==map[i][j+1]){
+                    cnt++;
+                    max = Math.max(max,cnt);
+                }else{
+                    cnt=1;
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){ // y 방향 진행
+            for(int j=0;j<n;j++){
+
+                if(isRange(i,j+1) && map[j][i]==map[j+1][i]){
+                    cnt++;
+                    max = Math.max(max,cnt);
+                }else{
+                    cnt=1;
+                }
+            }
+        }
+    }
+
+    static boolean isRange(int x,int y){
+        return x>=0&&y>=0&&x<n&&y<n;
+    }
+    static class Point{
+        int x,y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
