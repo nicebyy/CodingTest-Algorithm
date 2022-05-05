da95cb6fc6861ed6bad6d3ba48b04dd234ac9d33
nicebyy <45655434+nicebyy@users.noreply.github.com>
2022-05-05 18:32:44 +0900

    [Silver III] Title: 후위 표기식2, Time: 136 ms, Memory: 14480 KB -BaekjoonHub

---
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int operandCount = Integer.parseInt(br.readLine());
+        HashMap<Character, Double> operands = new HashMap<>();
+        Stack<Double> stack = new Stack<>();
+
+        char[] equations = br.readLine().toCharArray();
+
+        for(int i=0;i<operandCount;i++){
+            double value = Double.parseDouble(br.readLine());
+            operands.put((char) ('A'+i),value);
+        } // end input
+
+        for (char ch : equations) {
+
+            if(!isOperator(ch)){
+                stack.push(operands.get(ch));
+            }else {
+
+                Double op1 = stack.pop();
+                Double op2 = stack.pop();
+                Double result = 0.00;
+                if(ch=='+'){
+                    result = op2+op1;
+                }else if(ch=='-'){
+                    result = op2-op1;
+                }else if(ch=='/'){
+                    result = op2/op1;
+                }else if(ch=='*'){
+                    result = op2*op1;
+                }
+
+                stack.push(result);
+            }
+
+        }
+
+        System.out.printf("%.2f",stack.pop());
+
+    }
+
+    static boolean isOperator(char ch){
+        return ch<'A'||ch>'Z';
+    }
+}
