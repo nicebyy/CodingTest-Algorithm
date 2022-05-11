26492d4ad8a17e635287de699e7f34ac6c768016
nicebyy <45655434+nicebyy@users.noreply.github.com>
2022-05-11 18:15:52 +0900

    [Gold II] Title: 청소년 상어, Time: 212 ms, Memory: 17796 KB -BaekjoonHub

--- 백준/Gold/19236. 청소년 상어/청소년 상어.java
+++ 백준/Gold/19236. 청소년 상어/청소년 상어.java
@@ -1,7 +1,4 @@
-import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import static java.util.Arrays.stream;
 
@@ -24,27 +21,14 @@ public class Main {
                 fish[fishNum] = new Fish(i, j, fishDir, true);
             }
         }
-        //상어가 첫번째 위치의 물고기 먹음.
+        //첫번째 위치 먹고 start
         int fishNum = map[0][0];
         fish[fishNum].alive = false;
         map[0][0] = -1;
-        dfs(0, 0, fish[fishNum].d, fishNum);
+        dfs(0, 0, fish[fishNum].dir, fishNum);
         System.out.println(max);
     }
-    
-    public static class Fish {
-        int x, y, d;
-        boolean alive;
 
-        public Fish(int x, int y, int d, boolean alive) {
-            this.x = x;
-            this.y = y;
-            this.d = d;
-            this.alive = alive;
-        }
-
-    }
-    
     //상어 위치 (x, y)
     public static void moveFish(int x, int y) {
         for (int i = 1; i <= 16; i++) {
@@ -53,13 +37,15 @@ public class Main {
             //이미 먹은 물고기인 경우
             if (!moveFish.alive) 
                 continue;
+
             //8방향 이동여부 확인
             for (int j = 0; j < 8; j++) {
-                int nextDir = (moveFish.d + j) >= 9 ? (moveFish.d + j) % 8 : (moveFish.d + j);
+                int nextDir = (moveFish.dir + j) >= 9 ? (moveFish.dir + j) % 8 : (moveFish.dir + j);
                 int nextX = moveFish.x + dx[nextDir];
                 int nextY = moveFish.y + dy[nextDir];
 
-                if (nextX < 0 || nextY < 0 || nextX >= 4 || nextY >= 4 || (nextX == x && nextY == y)) continue;
+                if (!isRange(nextX,nextY)|| (nextX == x && nextY == y))
+                    continue;
 
                 if (map[nextX][nextY] != -1) {
                     Fish otherFish = fish[map[nextX][nextY]];
@@ -69,14 +55,14 @@ public class Main {
                     otherFish.y = moveFish.y;
                     moveFish.x = nextX;
                     moveFish.y = nextY;
-                    moveFish.d = nextDir;
+                    moveFish.dir = nextDir;
 
                 } else {
                     map[moveFish.x][moveFish.y] = -1;
                     map[nextX][nextY] = i;
                     moveFish.x = nextX;
                     moveFish.y = nextY;
-                    moveFish.d = nextDir;
+                    moveFish.dir = nextDir;
                 }
                 break;
             }
@@ -91,7 +77,7 @@ public class Main {
         //fish, map 상태 저장해둠
         Fish[] cloneFish = new Fish[17];
         for (int i = 1; i < 17; i++)
-            cloneFish[i] = new Fish(fish[i].x, fish[i].y, fish[i].d, fish[i].alive);
+            cloneFish[i] = new Fish(fish[i].x, fish[i].y, fish[i].dir, fish[i].alive);
         int[][] cloneMap = new int[4][4];
         for (int i = 0; i < 4; i++) {
             cloneMap[i] = map[i].clone();
@@ -101,13 +87,15 @@ public class Main {
         for (int i = 1; i < 4; i++) { // 상어 이동
             int nextX = x + dx[d] * i;
             int nextY = y + dy[d] * i;
-            if (!isRange(nextX,nextY)) break;
+
+            if (!isRange(nextX,nextY))
+                continue;
             //살아있는 물고기가 있는 경우
             int fishNum = map[nextX][nextY];
             if (fishNum != -1) {
                 fish[fishNum].alive = false;
                 map[nextX][nextY] = -1;
-                dfs(nextX, nextY, fish[fishNum].d, sum + fishNum);
+                dfs(nextX, nextY, fish[fishNum].dir, sum + fishNum);
                 fish[fishNum].alive = true;
                 map[nextX][nextY] = fishNum;
             }
@@ -120,9 +108,18 @@ public class Main {
     }
 
     static boolean isRange(int x,int y){
-
         return x>=0&&y>=0&&x<4&&y<4;
     }
 
+    public static class Fish {
+        int x, y, dir;
+        boolean alive;
 
+        public Fish(int x, int y, int dir, boolean alive) {
+            this.x = x;
+            this.y = y;
+            this.dir = dir;
+            this.alive = alive;
+        }
+    }
 }
