bcf7fb408fdab591c3108c3d91518286dd2da8a4
nicebyy <45655434+nicebyy@users.noreply.github.com>
2022-05-29 23:57:22 +0900

    [Silver I] Title: 동물원, Time: 144 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+import java.util.stream.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[n][3];
+        
+        dp[0][0] = 1;
+        dp[0][1] = 1;
+        dp[0][2] = 1;
+        
+        for(int i=1;i<n;i++){
+            dp[i][0] = (dp[i-1][0]+dp[i-1][1]+dp[i-1][2])%9901;
+            dp[i][1] = (dp[i-1][0]+dp[i-1][2])%9901;
+            dp[i][2] = (dp[i-1][0]+dp[i-1][1])%9901;
+        }
+        System.out.println(Arrays.stream(dp[n-1]).sum()%9901);
+    }
+}
