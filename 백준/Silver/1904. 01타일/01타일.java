a92a26ec298d22cb3a6d494ae5e372396b983feb
nicebyy <45655434+nicebyy@users.noreply.github.com>
2023-10-04 12:37:32 +0900

    [Silver III] Title: 01타일, Time: 108 ms, Memory: 19372 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -1,28 +1,27 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-	public static void main(String args[]) throws IOException {	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int cnt = Integer.parseInt(br.readLine());
 
-		long arr[] = new long[1000002];
-		arr[0]=0;arr[1]=1;
-		for(int i=0;i<cnt;i++)
-		{
-			arr[i+2] = arr[i] + arr[i+1];
-			arr[i+2] %=15746;
-		}
-//		System.out.println((arr[cnt]+arr[cnt-1])%15746);
-		System.out.println(arr[cnt+1]);
-		
-		}	
-	
+    static int n;
+    static long[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        dp = new long[1000001];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3;i<=n;i++){
+            dp[i] = (dp[i-1] + dp[i-2])%15746;
+        }
+
+        System.out.println(dp[n]);
+    }
 }
-
-
-
-
-
-
