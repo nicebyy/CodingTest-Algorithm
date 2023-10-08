18ec21454e56dc711cf13b0ead7961dede33744e
nicebyy <45655434+nicebyy@users.noreply.github.com>
2023-10-08 15:02:31 +0900

    [Silver IV] Title: 동전 0, Time: 276 ms, Memory: 18784 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,29 +1,32 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-	 
-	public class Main {
-	 
-	    public static void main(String[] args) throws NumberFormatException, IOException {
-	    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    	
-	       String input [] = br.readLine().split(" ");
-	       int cnt = Integer.parseInt(input[0]);
-	       int total = Integer.parseInt(input[1]);
-	       int [] token = new int[cnt];
-	       for(int i=0;i<cnt;i++)
-	    	  token[i]=Integer.parseInt(br.readLine());
-	       int min=0;
-	       int i=0;
-	       while(total>0&&(token.length-1-i)>=0)
-	       {
-	    	   if(total-token[token.length-1-i]>=0)
-	    		   {total = total-token[token.length-1-i];min++;}
-	    	   else
-	    		   i++;
-	    	   
-	       }
-	       System.out.println(min);
-	 
-   }
-	 
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] input = Arrays.stream(br.readLine().split(" "))
+                 .mapToInt(Integer::parseInt).toArray();
+
+        int n = input[0];
+        int k = input[1];
+        int[] coins = new int[n];
+        for(int i=0;i<n;i++)
+            coins[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(coins);
+
+        int cnt = 0;
+        for(int i=coins.length-1;i>=0;i--){
+
+            if(k - coins[i] >= 0){
+                k -= coins[i];
+                cnt++;
+                i++;
+            }
+        }
+        System.out.println(cnt);
+    }
 }
