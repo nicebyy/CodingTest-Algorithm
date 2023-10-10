1824cec899405e2ec9ab72e9537695bacf715e78
nicebyy <45655434+nicebyy@users.noreply.github.com>
2023-10-10 15:17:33 +0900

    [level 2] Title: 가장 큰 수, Time: 4.37 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,49 +1,15 @@
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.PriorityQueue;
+import java.util.Collections.*;
 import java.util.stream.Collectors;
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-
-        PriorityQueue<CustomNumber> pq = Arrays.stream(numbers)
-                .mapToObj(CustomNumber::new)
-                .collect(Collectors.toCollection(PriorityQueue::new));
-
-        StringBuilder sb = new StringBuilder();
-        
-        if(pq.peek().prefix == 0)
-            return "0";
-        while (!pq.isEmpty()){
-            sb.append(pq.poll().number);
-        }
-
-        return sb.toString();
-    }
-
-    static class CustomNumber implements Comparable<CustomNumber> {
-
-        int prefix;
-        String number;
-        String[] numberSplit;
-
-        public CustomNumber(int num) {
-            this.number = Integer.toString(num);
-            this.numberSplit = number.split("");
-            this.prefix = Integer.parseInt(numberSplit[0]);
-        }
-
-        @Override
-        public int compareTo(CustomNumber o) {
-
-            if(o.prefix == this.prefix){
-                return Integer.parseInt(o.number + this.number) - Integer.parseInt(this.number+o.number);
-            }
-            return o.prefix - this.prefix;
-        }
-
+        List<String> result = Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .sorted((s1, s2) -> (s2 + s1).compareTo(s1 + s2))
+                .collect(Collectors.toList());
+        return result.get(0).equals("0") ? "0" : String.join("", result);
     }
 }
