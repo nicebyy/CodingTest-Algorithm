614003b44395383a733e344ba94cd35a16c2d8f9
nicebyy <45655434+nicebyy@users.noreply.github.com>
2023-12-12 14:18:50 +0900

    [level 3] Title: 인사고과, Time: 99.72 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -1,38 +1,22 @@
-
 import java.util.*;
-import java.util.List;
-import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[][] scores) {
-
-        final int[] target = scores[0];
-
-        int rank = 1;
-        for(int i=1;i<scores.length;i++) {
-
-//            boolean failCond = false;
-            if(scores[i][0] + scores[i][1] > target[0] + target[1]){
-
-
-                if(checkCond(scores, i)){
-                    rank++;
-                }
+        int[] s = scores[0];
+        Arrays.sort(scores, (s1, s2) -> s1[0] == s2[0] ? s1[1] - s2[1] : s2[0] - s1[0]);
+        ArrayList<Integer> arr = new ArrayList<>();
+        int max = 0, ans = 1;
+        for(int[] score : scores) {
+            if(score[1] < max) {
+                if(score.equals(s))
+                    return -1;
             }
-
-            if(scores[i][0] > target[0] && scores[i][1] > target[1])
-                return -1;
-        }
-        return rank;
-    }
-
-    private static boolean checkCond(int[][] scores,int i) {
-
-        for(int j = 0; j< scores.length; j++) {
-            if( scores[j][0]  > scores[i][0] && scores[j][1] > scores[i][1]){
-                return false;
+            else {
+                max = Math.max(max, score[1]);
+                if(s[0] + s[1] < score[0] + score[1])
+                    ans++;
             }
         }
-        return true;
+        return ans;
     }
 }
