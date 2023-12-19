09f2d3f3a715bd1f28c5ffc02847b0729d3a3c38
nicebyy <45655434+nicebyy@users.noreply.github.com>
2023-12-19 13:44:13 +0900

    [Gold II] Title: 반도체 설계, Time: 264 ms, Memory: 21508 KB -BaekjoonHub

--- 백준/Gold/2352. 반도체 설계/반도체 설계.java
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -1,10 +1,7 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
 
 import static java.util.Arrays.stream;
 
@@ -13,7 +10,7 @@ public class Main {
 
     static int n;
     static int[] arr;
-    static int[] dp;
+    static int[] lis;
 
     public static void main(String[] args) throws IOException {
 
@@ -21,20 +18,35 @@ public class Main {
         arr = stream(br.readLine().split(" "))
                             .mapToInt(Integer::parseInt)
                             .toArray();
-        dp = new int[arr.length];
-        Arrays.fill(dp,1);
-        dp[0] = 1;
+        lis = new int[arr.length];
+        lis[0] = arr[0];
+        int index = 0;
+        for(int i=1;i<lis.length;i++){
 
-        for(int i=1;i<arr.length;i++){
+            if(lis[index] < arr[i]){
+                lis[++index] = arr[i];
+            }else if(lis[index] > arr[i]){
 
-            for(int j=0;j<i;j++){
-                if(arr[i] > arr[j]){
-                    dp[i] = Math.max(dp[i],dp[j]+1);
+                int start = 0;
+                int end = index;
+
+                while (start < end){
+
+                    int mid = (start + end)/2;
+
+                    if(lis[mid] < arr[i]){
+                        start = mid+1;
+                    }else{
+                        end = mid;
+                    }
                 }
+
+                lis[end] = arr[i];
             }
         }
 
-        System.out.println(Arrays.stream(dp).max().getAsInt());
+        System.out.println(index+1);
+
     }
 }
 
