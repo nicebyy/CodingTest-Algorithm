85bb3537db3d96c9c8853284295d3b768e4696de
nicebyy <45655434+nicebyy@users.noreply.github.com>
2024-07-25 16:19:30 +0900

    [Gold V] Title: LCS, Time: 220 ms, Memory: 28912 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,27 +1,45 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
 
 public class Main {
 
+    static char[] arr1, arr2;
+    static Integer[][] dp;
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] arr1 = (" "+br.readLine()).toCharArray();
-        char[] arr2 = (" "+br.readLine()).toCharArray();
-        int[][] dp = new int[arr1.length+1][arr2.length+1];
 
-        for(int i=1;i<arr1.length;i++){
-            for(int j=1;j<arr2.length;j++){
+        arr1 = br.readLine().toCharArray();
+        arr2 = br.readLine().toCharArray();
 
-                if(arr1[i] == arr2[j]){
-                    dp[i][j] = dp[i-1][j-1]+1;
-                }else{
-                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
-                }
-            }
+        dp = new Integer[arr1.length][arr2.length];
+
+        int result = go(arr1.length - 1, arr2.length - 1);
+        System.out.println(result);
+    }
+
+    static int go(int i, int j) {
+
+        if (i < 0 || j < 0) {
+            return 0;
         }
 
-        System.out.println(dp[arr1.length-1][arr2.length-1]);
+        if(dp[i][j] == null){
+            dp[i][j] = 0;
+            if (arr1[i] == arr2[j]) {
+                dp[i][j] = go(i - 1, j - 1) + 1;
+            } else {
+                dp[i][j] = Math.max(go(i - 1, j), go(i, j - 1));
+            }
+        }
+        return dp[i][j];
     }
 }
+
+
