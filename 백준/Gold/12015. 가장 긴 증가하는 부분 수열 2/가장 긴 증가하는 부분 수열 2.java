de73f0746694d1ecfc67d9ed7575639c60cc0ed9
nicebyy <45655434+nicebyy@users.noreply.github.com>
2025-02-05 13:21:55 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 500 ms, Memory: 114496 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -2,80 +2,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static java.util.Arrays.stream;
 
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
 
         int n = Integer.parseInt(br.readLine());
         int[] arr = stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+                            .mapToInt(Integer::parseInt)
+                            .toArray();
         int[] lis = new int[n];
-
-        int index = 1;
         lis[0] = arr[0];
 
-        for (int i = 1; i < arr.length; i++) {
+        int index = 0;
+        for(int i=1;i<n;i++){
 
-            int last = lis[index - 1];
-//            System.out.println("key : " + arr[i]);
-            if (arr[i] > last) {
-                index++;
-                lis[index - 1] = arr[i];
-            } else {
-
-                int start = 0;
-                int end = index - 1;
-
-                while (start < end) {
-
-                    int mid = (start + end) / 2;
-
-//                    print(start, end, mid, lis, index);
-
-                    if (lis[mid] >= arr[i]) {
-                        end = mid;
-                    } else {
-                        start = mid + 1;
-                    }
-                }
-                lis[end] = arr[i];
+            if(lis[index] < arr[i]){
+                lis[++index] = arr[i];
+            }else{
+                int pos = binarySearch(lis, index, arr[i]);
+                lis[pos] = arr[i];
             }
-
-//            System.out.println(Arrays.toString(lis));
         }
 
-        System.out.println(index);
+        System.out.println(index+1);
     }
+    private static int binarySearch(int[] lis, int index, int target) {
 
-    public static void print(int start, int end, int mid, int[] arr, int arrLength) {
+        int left = 0;
+        int right = index+1;
 
-        System.out.print("start = " + start);
-        System.out.print("    mid = " + mid);
-        System.out.print("    end = " + end + "     [");
+        while (left < right){
 
-        for (int i = 0; i < arrLength; i++) {
-            System.out.print(arr[i] + " ");
+            int mid = (left + right) / 2;
+            
+            if(lis[mid] == target){
+                return mid;
+            }else if(lis[mid] > target){
+                right = mid;
+            }else{
+                left = mid+1;
+            }
         }
-        System.out.println("]");
+        return left;
     }
-
 }
-/**
- * 4 7 10 3 1 8 7 2 5 7
- * <p>
- * 4
- * 4 7
- * 4 7 10
- * 3 7 10
- * 1 7 10
- * 2 7 8
- */
-
