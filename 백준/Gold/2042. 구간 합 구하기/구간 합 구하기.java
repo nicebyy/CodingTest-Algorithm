94bd76c1addde82c925680e210f82b259a384885
nicebyy <45655434+nicebyy@users.noreply.github.com>
2025-03-07 15:15:08 +0900

    [Gold I] Title: 구간 합 구하기, Time: 812 ms, Memory: 172468 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -2,88 +2,113 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.math.BigInteger;
 
 import static java.util.Arrays.stream;
 
 public class Main {
-    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    static int n,queryCount,changeCount;
-    static long[] arr,tree;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static long[] array;
     public static void main(String[] args) throws IOException {
-
         int[] input = stream(br.readLine().split(" "))
                             .mapToInt(Integer::parseInt)
                             .toArray();
-
-        n = input[0];
-        changeCount = input[1];
-        queryCount = input[2];
-        int h = (int) Math.ceil(Math.log(n)/Math.log(2));
-        tree = new long[(int) Math.pow(2,h+1)];
-        arr = new long[n+1];
-
-        for(int i=1;i<=n;i++){
-            arr[i] = Long.parseLong(br.readLine());
+        array = new long[input[0]];
+        for(int i=0; i<array.length; i++){
+            array[i] = Long.parseLong(br.readLine());
         }
+        TreeNode root = buildTree(array, 0, array.length-1);
 
-        setTree(1,1,n);
-
-        for(int q=0; q<queryCount+changeCount; q++){
-
+        for(int i=0; i<input[1] + input[2]; i++){
             long[] query = stream(br.readLine().split(" "))
                                 .mapToLong(Long::parseLong)
                                 .toArray();
             if(query[0] == 1){
-
-                int targetIndex = Math.toIntExact(query[1]);
-                long updateValue = query[2];
-
-                long diff = updateValue - arr[targetIndex];
-                arr[targetIndex] = updateValue;
-                updateTree(1,1,n,targetIndex,diff);
+                update(root, Math.toIntExact(query[1]-1), query[2]);
             }else{
-                long sum = getTreeSum(1,1, n,query[1],query[2]);
+                long sum = getSum(root, Math.toIntExact(query[1]-1), Math.toIntExact(query[2]-1));
                 System.out.println(sum);
             }
+
+//            System.out.println(root.sum);
         }
     }
 
-    public static long getTreeSum(int node, int start, int end,long left,long right){
+    static void update(TreeNode node, int index, long value){
+        if(node.start == node.end){
+            node.sum = value;
+        }else{
+            int mid = (node.start + node.end)/2;
 
-        if(left > end || right < start){
-            return 0;
-        }
-        if(left <= start && right >= end){
-            return tree[node];
-        }
-        return getTreeSum(node*2, start, (start+end)/2, left, right)+
-                getTreeSum(node*2+1, (start+end)/2+1, end, left, right);
-    }
-    public static void updateTree(int node,int start,int end,int targetIndex, long diff) {
-
-        if(targetIndex >= start && targetIndex <= end){
-            tree[node] = tree[node] + diff;
-
-            if(start != end){
-                updateTree(node*2,start, (start+end)/2, targetIndex, diff);
-                updateTree(node*2+1,(start+end)/2+1, end, targetIndex, diff);
+            if(mid >= index){
+                update(node.left, index, value);
+            }else{
+                update(node.right, index, value);
             }
+            node.sum = node.left.sum + node.right.sum;
         }
     }
 
-    public static long setTree(int node,int start,int end){
+    static long getSum(TreeNode node, int from, int to){
+
+        if(node.start == from && node.end == to){
+            return node.sum;
+        }
+
+        int mid = (node.start + node.end)/2;
+
+        if(to <= mid) {
+            return getSum(node.left, from, to);
+        } else if(from > mid) {
+            return getSum(node.right, from, to);
+        } else {
+            return getSum(node.left, from, mid) + getSum(node.right, mid+1, to);
+        }
+    }
+
+    static TreeNode buildTree(long[] array, int start, int end){
+
+        TreeNode node = new TreeNode(start, end, 0);
 
         if(start == end){
-            tree[node]  = arr[start];
+            node.sum = array[start];
         }else{
-            long leftSum = setTree(node * 2, start, (start + end) / 2);
-            long rightSum = setTree(node * 2+1, (start+end)/2+1, end);
-            tree[node] = leftSum + rightSum;
+
+            int mid = (start + end)/2;
+            node.left = buildTree(array, start, mid);
+            node.right = buildTree(array, mid+1, end);
+
+            node.sum += node.left!=null ? node.left.sum : 0;
+            node.sum += node.right!= null ? node.right.sum : 0;
         }
-        return tree[node];
+        return node;
+    }
+
+    static class TreeNode{
+        int start = 0;
+        int end = 0;
+        long sum = 0;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int start, int end, long sum) {
+            this.start = start;
+            this.end = end;
+            this.sum = sum;
+        }
     }
 }
 
-
+/**
+ * 5 2 2
+ * 1
+ * 2
+ * 3
+ * 4
+ * 5
+ * 1 3 6
+ * 2 2 5
+ * 1 5 2
+ * 2 3 5
+ */
