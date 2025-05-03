c25c644b8856c289cd0b4d4e3ae2c5e9626a560b
nicebyy <45655434+nicebyy@users.noreply.github.com>
2025-05-03 13:51:19 +0900

    [Gold II] Title: 계보 복원가 호석, Time: 908 ms, Memory: 180564 KB, Score: 9 point -BaekjoonHub

---
+++ 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.kt
@@ -0,0 +1,50 @@
+import java.util.LinkedList
+import java.util.PriorityQueue
+import java.util.TreeMap
+
+var map = HashMap<String, ArrayList<String>>()
+var graph = TreeMap<String, PriorityQueue<String>>()
+var inDegree = HashMap<String, Int>()
+val answer = StringBuilder()
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val names = readLine().split(" ")
+    names.forEach { name ->
+        inDegree[name] = 0
+        map[name] = ArrayList()
+        graph[name] = PriorityQueue()
+    }
+
+    val m = readLine().toInt()
+    repeat(m) {
+        val (from, to) = readLine().split(" ")
+        inDegree[from] = inDegree[from]!! +1
+        map[to]!!.add(from)
+    }
+    val rootList = ArrayList<String>()
+    inDegree
+        .filter { (_, value) ->  value == 0}
+        .forEach { (name,_)->rootList.add(name) }
+    val q = LinkedList<String>()
+    q.addAll(rootList)
+
+
+    while (q.isNotEmpty()){
+
+        val poll = q.poll() !!
+        for (next in map[poll]!!) {
+            inDegree[next] = inDegree[next]!!-1
+            if(inDegree[next] == 0){
+                graph[poll]!!.add(next)
+                q.add(next)
+            }
+        }
+    }
+
+    answer.append("${rootList.size}\n")
+        .append("${rootList.joinToString(" ")}\n")
+    graph.forEach { (key, value) ->
+        answer.append("$key ${value.size} ${value.toCollection(ArrayList<String>()).joinToString(" ")}\n")
+    }
+    print(answer)
+}
